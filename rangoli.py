a4a5465a70ecd9baa6342d63c998d26a0be71009
Rahul Kumar Jha <67184791+kumarjhar@users.noreply.github.com>
2020-10-03 17:22:01 +0530

    Update rangoli.py
    I have changed the small algorithm in your code now its more optimized.

--- rangoli.py
+++ rangoli.py
@@ -1,3 +1,4 @@
+'''
 n=int(input("Enter"))
 L=[]
 for x in range(n):
@@ -5,16 +6,39 @@ for x in range(n):
         pattern=chr(97+n-y+1)
         L.append(pattern)
         print(pattern)
+'''
 
+'''
 n=int(input("Enter"))
 L=[]
 for x in range(n):
     pattern=chr(ord('a')+n-y-1) for y in range(x+1)
     print(pattern)
-
+'''
+'''
 n=int(input("Enter"))
 L=[]
 for x in range(n):
     pattern='-'.join(chr(ord('a')+n-y-1) for y in range(x+1))
     print(pattern)
+'''
+n=int(input("Enter"))
+local=n
+L=[]
+for x in range(n):
+    pattern='-'.join(chr(ord('a')+n-y-1) for y in range(x+1))
+    print((pattern+pattern[::-1][1:]).center(4*n-3,'-')) #explain the pattern of max length
 
+for x in range(n-1):
+    pattern='-'.join(chr(ord('a')+n-y-1) for y in range(n-x-1))
+    print((pattern+pattern[::-1][1:]).center(4*n-3,'-')) #explain the pattern of max length
+
+
+"""
+n=1 : 1
+n=2 : 3
+n=3 : 9
+n=4 : 7
+
+
+"""
